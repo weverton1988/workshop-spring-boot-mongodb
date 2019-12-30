515893f271bf9e7ea742547cd6ab26ac49a29a5b
wtonja@gmail.com <wtonja@gmail.com>
2019-12-30 14:07:08 -0300

    Inserção de usuário com POST

--- src/main/java/com/weverton/workshopmongo/services/UserService.java
+++ src/main/java/com/weverton/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.weverton.workshopmongo.domain.User;
+import com.weverton.workshopmongo.dto.UserDTO;
 import com.weverton.workshopmongo.repository.UserRepository;
 import com.weverton.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
