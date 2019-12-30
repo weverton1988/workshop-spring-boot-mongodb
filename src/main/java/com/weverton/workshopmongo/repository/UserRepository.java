516354ff4dd98f59063b0a906504252944d14e5e
wtonja@gmail.com <wtonja@gmail.com>
2019-12-30 10:33:58 -0300

    Operação de instanciação da base de dado

--- src/main/java/com/weverton/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/weverton/workshopmongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.weverton.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.weverton.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+	void save(List<User> asList);
 	
 }
