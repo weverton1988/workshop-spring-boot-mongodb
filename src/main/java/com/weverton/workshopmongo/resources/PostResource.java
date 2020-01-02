948a3e7e7fe8e393ac627a6a8eb7bcfaf1314324
wtonja@gmail.com <wtonja@gmail.com>
2020-01-02 10:16:14 -0300

    Obtendo um post por id

---
+++ src/main/java/com/weverton/workshopmongo/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.weverton.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.weverton.workshopmongo.domain.User;
+import com.weverton.workshopmongo.services.UserService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private UserService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Object findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+
+}
---
+++ src/main/java/com/weverton/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.weverton.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.weverton.workshopmongo.domain.Post;
+import com.weverton.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		 Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow();
+	}
+	
+}
